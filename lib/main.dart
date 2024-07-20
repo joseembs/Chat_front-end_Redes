c9b4119bbc5a9ae2697903ce3e858c8c636b5d1b
José Edson MBS <joseedsonmbs04@gmail.com>
2024-07-20 19:09:28 -0300

    Add cadastro e login básico do usuário

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'dart:convert';
 
 import 'package:flutter/material.dart';
 import 'API.dart';
-import 'dart:convert';
 
 void main() {
   runApp(const MyApp());
@@ -44,9 +43,18 @@ class _MyHomePageState extends State<MyHomePage> {
   late String url;
   late var info;
 
+  String nome = "";
   String email = "";
   String msg = "";
   String rcv = "Aguardando mensagem...";
+  String userCheck = "Entrar";
+
+  void _setNome(userNome) {
+    setState(() {
+      nome = userNome;
+      print(nome);
+    });
+  }
 
   void _setEmail(userEmail) {
     setState(() {
@@ -72,54 +80,131 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
       body: Center(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            email == ""
-                ? Text(
-              'Informe seu email:',
-            )
-                : Text('Seu email é:'),
-            Container(
-              width: 200,
-              child: TextField(
-                onChanged: (String newEmail) async {
-                  _setEmail(newEmail);
-                },
-              ),
-            ),
+          children: [
+            SizedBox(height: 50),
+            _Login(),
             SizedBox(height: 100),
-            Text('Envie sua mensagem:'),
-            Container(
-              width: 400,
-              child: TextField(
-                onChanged: (String newMsg) async {
-                  _setMsg(newMsg);
-                },
-              ),
-            ),
-            SizedBox(height: 50),
-            TextButton(
-                onPressed: () async {
-                  url = 'http://127.0.0.1:5000/api?pedido=sendMsg&email=' + email + '&mensagem=' + msg;
-                  print(url);
-
-                  info = await GetData(url);
-                  print(info);
-
-                  var decodedInfo = jsonDecode(info);
-
-                  setState(() {
-                    rcv = decodedInfo['mensagem'];
-                  });
-
-                  print('enviou');
-                },
-                child: Text("Enviar mensagem")),
-            SizedBox(height: 50),
-            Text(rcv),
+            _buildChat(),
           ],
         ),
       ),
     );
   }
+
+  Widget _Login() {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Text(
+          'Informe seu nome:',
+        ),
+        Container(
+          width: 200,
+          child: TextField(
+            onChanged: (String newNome) async {
+              _setNome(newNome);
+            },
+          ),
+        ),
+        SizedBox(height: 50),
+        Text(
+          'Informe seu email:',
+        ),
+        Container(
+          width: 200,
+          child: TextField(
+            onChanged: (String newEmail) async {
+              _setEmail(newEmail);
+            },
+          ),
+        ),
+        SizedBox(height: 50),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextButton(
+                onPressed: () async {
+                  url =
+                      'http://127.0.0.1:5000/api?pedido=cadastro&email='+email;
+                  print(url);
+
+                  info = await GetData(url);
+                  print(info);
+
+                  // var responseInfo = jsonDecode(info);
+
+                },
+                child: Text("Cadastrar usuário")),
+            TextButton(
+                onPressed: () async {
+                  url = 'http://127.0.0.1:5000/api?pedido=login&email='+email;
+                  print(url);
+
+                  info = await GetData(url);
+                  print(info);
+
+                  var responseInfo = jsonDecode(info);
+
+                  setState(() {
+                    if(responseInfo['cadastrado'] == true){
+                      userCheck = "Usuário encontrado";
+                      print("foi");
+                    } else {
+                      userCheck = "Usuário não existe";
+                      print("não foi");
+                    }
+                  });
+                },
+                child: Text(userCheck)),
+          ],
+        ),
+      ],
+    );
+  }
+
+  Widget _buildChat() {
+    return Column(
+      children: [
+        Text('Envie sua mensagem:'),
+        Container(
+          width: 400,
+          child: TextField(
+            onChanged: (String newMsg) async {
+              _setMsg(newMsg);
+            },
+          ),
+        ),
+        SizedBox(height: 50),
+        TextButton(
+            onPressed: () async {
+              url = 'http://127.0.0.1:5000/api?pedido=sendMsg&email=' + email + '&mensagem=' + msg;
+              print(url);
+
+              info = await GetData(url);
+              print(info);
+
+              var responseInfo = jsonDecode(info);
+
+              setState(() {
+                rcv = responseInfo['mensagem'];
+              });
+
+              print('enviou');
+            },
+            child: Text("Enviar mensagem")),
+        SizedBox(height: 50),
+        Text(rcv),
+      ],
+    );
+  }
+
+  // Widget _getChats() {
+  //   return SizedBox();
+  // }
+
+  // _getResponseJson(String urlIn) async {
+  //   info = await GetData(url);
+  //   print(info);
+  //   return jsonDecode(info);
+  // }
 }
