4d054791aad8d770a19b1d9c9ed98f2778b4a717
José Edson MBS <joseedsonmbs04@gmail.com>
2024-07-25 01:50:01 -0300

    Mudança de http para socket

--- lib/API.dart
+++ lib/API.dart
@@ -1,3 +1,39 @@
+import 'dart:convert';
+import 'dart:io';
+
+Future<Map<String, String>> toFromServer(var jsonIn, {int sockNum = 5000}) async {
+  var socket = await Socket.connect('localhost', sockNum);
+
+  var jsonString = jsonEncode(jsonIn);
+  socket.write(jsonString);
+
+  socket.listen((List<int> received) {
+    var response = utf8.decode(received);
+    var decodedResponse = jsonDecode(response);
+    print(decodedResponse);
+    return decodedResponse;
+  });
+
+  return {"error": "erro"};
+}
+
+//await Future.delayed(Duration(seconds: 2));
+// socket.destroy();
+/*
+void sendTo() async {
+  var socket = await Socket.connect('localhost', 4040);
+  var jsonData = {
+    'name': 'John Doe',
+    'email': 'john.doe@example.com'
+  };
+  var jsonString = jsonEncode(jsonData);
+  socket.write(jsonString);
+  socket.destroy();
+  print('Data sent to server.');
+}
+ */
+
+/*
 import 'package:http/http.dart' as http;
 
 Future GetData (url) async{
@@ -7,5 +43,8 @@ Future GetData (url) async{
   // });
   final response = await http.get(Uri.parse(url));
 
+  //1º envia ip servidor, socket base -> recebe um socket específico
+  //2º envia ip servidor, socket específico + pedido codificado
+
   return response.body;
-}
+}*/
